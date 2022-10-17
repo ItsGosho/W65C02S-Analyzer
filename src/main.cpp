23421dc0099dff3116847e63b372d201ec144547
itsgosho <itsgosho2@gmail.com>
2022-10-18 00:10:08 +0300

    Pin initialization

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
 #include <Arduino.h>
 
+int mpAddressPins[16] = {22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52};
+int mpDataPins[8] = {31, 33, 35, 37, 39, 41, 43, 45};
+
+template<typename T, size_t N>
+void setPinsMode(T (& pinNumbers)[N], const byte& mode) {
+    for (size_t i = 0; i < N; i++) {
+        pinMode(pinNumbers[i], mode);
+    }
+}
+
+
 void setup() {
-// write your initialization code here
+    Serial.begin(9600);
+
+    setPinsMode(mpAddressPins, INPUT);
+    setPinsMode(mpDataPins, INPUT);
 }
 
 void loop() {
