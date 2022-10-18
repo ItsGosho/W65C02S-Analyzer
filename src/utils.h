59280635d62b3325e8b762863c27dd6f7b0b9658
itsgosho <itsgosho2@gmail.com>
2022-10-19 02:46:09 +0300

    Option to read multiple pins - lsb

--- src/utils.h
+++ src/utils.h
@@ -26,6 +26,28 @@ namespace itsgosho {
 
         return result;
     }
+
+    /**
+     * Will read the data from the digital pins and construct a decimal number.
+     *
+     * Example:
+     *
+     * Digital Pin(State):
+     * 5(1), 3(0), 6(1), 4(1)
+     *
+     * int pins[4] = {5, 3, 6, 4};
+     * digitalRead(pins, LSB); -> 13 (1101)
+     */
+    template<typename T, size_t N>
+    unsigned long digitalReadLSB(T (& pinNumbers)[N]) {
+
+        unsigned long result = 0;
+
+        for (int i = N - 1; i >= 0; --i)
+            result = (result << 1) | digitalRead(pinNumbers[i]);
+
+        return result;
+    }
 }
 
 
