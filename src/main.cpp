12fa5371bde02596da41f47dd76c1fd968d43454
itsgosho <itsgosho2@gmail.com>
2022-10-20 06:36:08 +0300

    Added a op codes names array, used to visualize the op code from the read address's data

--- src/main.cpp
+++ src/main.cpp
@@ -35,10 +35,28 @@ using namespace itsgosho;
 int mpAddressPins[16] = {MP_A0_PIN, MP_A1_PIN, MP_A2_PIN, MP_A3_PIN, MP_A4_PIN, MP_A5_PIN, MP_A6_PIN, MP_A7_PIN, MP_A8_PIN, MP_A9_PIN, MP_A10_PIN, MP_A11_PIN, MP_A12_PIN, MP_A13_PIN, MP_A14_PIN, MP_A15_PIN};
 int mpDataPins[8] = {MP_D0_PIN, MP_D1_PIN, MP_D2_PIN, MP_D3_PIN, MP_D4_PIN, MP_D5_PIN, MP_D6_PIN, MP_D7_PIN};
 
+static char* opCodeNames[0xFF + 1] = {
+        /*0*/ "BRK", "ORA", nullptr, nullptr, "TSB", "ORA", "ASL", "RMB0", "PHP", "ORA", "ASL", nullptr, "TSB", "ORA", "ASL", "BBR0",
+        /*1*/ "BPL", "ORA", "ORA", nullptr, "TRB", "ORA", "ASL", "RMB1", "CLC", "ORA", "INC", nullptr, "TRB", "ORA", "ASL", "BBR1",
+        /*2*/ "JSR", "AND", nullptr, nullptr, "BIT", "AND", "ROL", "RMB2", "PLP", "AND", "ROL", nullptr, "BIT", "AND", "ROL", "BBR2",
+        /*3*/ "BMI", "AND", "AND", nullptr, "BIT", "AND", "ROL", "RMB3", "SEC", "AND", "DEC", nullptr, "BIT", "AND", "ROL", "BBR3",
+        /*4*/ "RTI", "EOR", nullptr, nullptr, nullptr, "EOR", "LSR", "RMB4", "PHA", "EOR", "LSR", nullptr, "JMP", "EOR", "LSR", "BBR4",
+        /*5*/ "BVC", "EOR", "EOR", nullptr, nullptr, "EOR", "LSR", "RMB5", "CLI", "EOR", "PHY", nullptr, nullptr, "EOR", "LSR", "BBR5",
+        /*6*/ "RTS", "ADC", nullptr, nullptr, "STZ", "ADC", "ROR", "RMB6", "PLA", "ADC", "ROR", nullptr, "JMP", "ADC", "ROR", "BBR6",
+        /*7*/ "BVS", "ADC", "ADC", nullptr, "STZ", "ADC", "ROR", "RMB7", "SEI", "ADC", "PLY", nullptr, "JMP", "ADC", "ROR", "BBR7",
+        /*8*/ "BRA", "STA", nullptr, nullptr, "STY", "STA", "STX", "SMB0", "DEY", "BIT", "TXA", nullptr, "STY", "STA", "STX", "BBS0",
+        /*9*/ "BCC", "STA", "STA", nullptr, "STY", "STA", "STX", "SMB1", "TYA", "STA", "TXS", nullptr, "STZ", "STA", "STZ", "BBS1",
+        /*A*/ "LDY", "LDA", "LDX", nullptr, "LDY", "LDA", "LDX", "SMB2", "TAY", "LDA", "TAX", nullptr, "LDY", "LDA", "LDX", "BBS2",
+        /*B*/ "BCS", "LDA", "LDA", nullptr, "LDY", "LDA", "LDX", "SMB3", "CLV", "LDA", "TSX", nullptr, "LDY", "LDA", "LDX", "BBS3",
+        /*C*/ "CPY", "CMP", nullptr, nullptr, "CPY", "CMP", "DEC", "SMB4", "INY", "CMP", "DEX", "WAI", "CPY", "CMP", "DEC", "BBS4",
+        /*D*/ "BNE", "CMP", "CMP", nullptr, nullptr, "CMP", "DEC", "SMB5", "CLD", "CMP", "PHX", "STP", nullptr, "CMP", "DEC", "BBS5",
+        /*E*/ "CPX", "SBC", nullptr, nullptr, "CPX", "SBC", "INC", "SMB6", "INX", "SBC", "NOP", nullptr, "CPX", "SBC", "INC", "BBS6",
+        /*F*/ "BEQ", "SBC", "SBC", nullptr, nullptr, "SBC", "INC", "SMB7", "SED", "SBC", "PLX", nullptr, nullptr, "SBC", "INC", "BBS7"};
+
 volatile uint16_t instructionCounter = 1;
 
 void onClockRisingEdge() {
-    char output[32];
+    char output[50];
 
     bool operation = digitalRead(MP_RWB_PIN);
     unsigned int address = digitalRead(mpAddressPins, LSBFIRST);
@@ -47,7 +65,14 @@ void onClockRisingEdge() {
     if (address == 0xffff)
         instructionCounter = 1;
 
-    sprintf(output, "%d. [%c] Address: %04x Data: %02x", instructionCounter, (operation ? 'R' : 'W'), address, data);
+    char* opCodeName = opCodeNames[data];
+    sprintf(output,
+            "%d. [%c] Address: %04x Data: %02x [%s]",
+            instructionCounter,
+            (operation ? 'R' : 'W'),
+            address,
+            data,
+            opCodeName != nullptr ? opCodeName : "Unknown OpCode!");
     Serial.println(output);
 
     instructionCounter++;
